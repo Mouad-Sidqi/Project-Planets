f57b63d2244829cb936c91bf0d03fc81b7fc2a74
Hamza Elbouazizi <helbouaz@e1r9p8.1337.ma>
2019-03-03 19:02:04 +0100

    ...

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	[SerializeField]
+	private int maxMonsters;
+
+	[SerializeField]
+	private float	spawningRadius;
+	[SerializeField]
+	private	float	timeBetweenSpawns;
+	[SerializeField]
+	private	GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void	SpawnEnemy()
+	{
+		Instantiate()
+	}
+
+}
